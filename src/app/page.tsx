b4ca85ae158d520e09907be6a96574084f0cd2c3
nicholascaalvin <100178532+nicholascaalvin@users.noreply.github.com>
2025-03-13 00:50:05 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,9 +111,9 @@ const App = () => {
       setUser(JSON.parse(Cookies.get("user_data") || "null").email);
       setUserType(JSON.parse(Cookies.get("user_data") || "null").type);
     }
-    console.log(process.env.BACKEND_API);
+    console.log(process.env.NEXT_PUBLIC_BACKEND_API);
     axios
-      .get(process.env.BACKEND_API + "/api/products", {
+      .get(process.env.NEXT_PUBLIC_BACKEND_API + "/api/products", {
         headers: {
           "ngrok-skip-browser-warning": true,
         },
